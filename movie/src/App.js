7199f74f0cb0cbffbfff9cd275f2a88833ee45dd
sung1842 <sungwoo0723@naver.com>
2024-05-26 23:22:07 +0900

    modified:   movie/src/App.js 	modified:   movie/src/components/Movie.js

--- movie/src/App.js
+++ movie/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail/>}/>
+        <Route path="/movie/:id" element={<Detail/>}/>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}/>
       </Routes>
     </Router>
